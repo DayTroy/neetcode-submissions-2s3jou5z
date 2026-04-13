2b734aaa895b64c8a0a58c1e0ed7569d331a0075
Rustam <70051506+DayTroy@users.noreply.github.com>
2026-04-13 21:36:50 +0300

    Add: two-integer-sum - submission-18

---
+++ Data Structures & Algorithms/two-integer-sum/submission-18.js
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums, target) {    
+        const indexedNums = nums.map((value, index) => ({ value, index }));
+    indexedNums.sort((a, b) => a.value - b.value);
+        let startIndex = 0, endIndex = nums.length - 1;
+
+        while(startIndex < endIndex) {
+            const sum = indexedNums[startIndex].value + indexedNums[endIndex].value;
+            if (sum === target) {
+                return [indexedNums[startIndex].index, indexedNums[endIndex].index];
+            }
+            else if (sum> target) {
+                endIndex--;
+            }
+            else if (sum < target) {
+                startIndex++;
+            }
+        }
+    }
+}
