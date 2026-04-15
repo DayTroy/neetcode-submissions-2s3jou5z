55f9b3420db789db7f8b735006720e7db06dcb75
Rustam <70051506+DayTroy@users.noreply.github.com>
2026-04-15 22:44:56 +0300

    Update: products-of-array-discluding-self - submission-4

--- Data Structures & Algorithms/products-of-array-discluding-self/submission-4.js
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-4.js
@@ -3,25 +3,23 @@ class Solution {
      * @param {number[]} nums
      * @return {number[]}
      */
-
     // [1,2,4,6]
     // [1,1,1,1]
-
     // [1,1,2,8]
+    // [1,24,6,1]
 
-    // [1,2,8,48]
-
+    // [1,24,12,8]
     productExceptSelf(nums) {
         const prefix = new Array(nums.length).fill(1);
         const suffix = new Array(nums.length).fill(1);
         const result = [];
 
         for (let i = 1; i < nums.length; i++) {
-            prefix[i] = nums[i - 1] * prefix[i - 1];
+            prefix[i] = prefix[i - 1] * nums[i - 1];
         }
 
         for (let i = nums.length - 2; i >= 0; i--) {
-            suffix[i] = nums[i + 1] * suffix[i + 1];
+            suffix[i] = suffix[i + 1] * nums[i + 1];
         }
 
         for (let i = 0; i < nums.length; i++) {
