fd1f19fe5b467d16827813146b3be65a4e1d78ad
Rustam <70051506+DayTroy@users.noreply.github.com>
2026-04-16 23:38:15 +0300

    Add: valid-sudoku - submission-3

---
+++ Data Structures & Algorithms/valid-sudoku/submission-3.js
@@ -0,0 +1,43 @@
+class Solution {
+    /**
+     * @param {character[][]} board
+     * @return {boolean}
+     */
+    isValidSudoku(board) {
+        for (let row of board) {
+            let seen = '';
+            for (let j = 0; j < row.length; j++) {
+                if (row[j] === '.') continue;
+                if (seen.includes(row[j])) return false;
+                seen += row[j];
+            }
+        }
+
+        for (let i = 0; i < board.length; i++) {
+            let seen = '';
+            for (let j = 0; j < board.length; j++) {
+                if (board[j][i] === '.') continue;
+                if (seen.includes(board[j][i])) return false;
+                seen += board[j][i];
+            }
+        }
+
+        for (let boxRow = 0; boxRow < 3; boxRow++) {
+            for (let boxCol = 0; boxCol < 3; boxCol++) {
+                let seen = '';
+                let startRow = boxRow * 3;
+                let startCol = boxCol * 3;
+
+                for (let k = startRow; k < startRow + 3; k++) {
+                    for (let l = startCol; l < startCol + 3; l++) {
+                        if (board[k][l] === '.') continue;
+                        if (seen.includes(board[k][l])) return false;
+                        seen += board[k][l];
+                    }
+                }
+            }
+        }
+
+        return true;
+    }
+}
