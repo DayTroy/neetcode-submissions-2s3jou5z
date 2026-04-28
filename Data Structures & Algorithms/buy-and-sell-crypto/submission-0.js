e8a1a19e79b6fff221e8fc4734b78760689403e4
Rustam <70051506+DayTroy@users.noreply.github.com>
2026-04-28 14:11:30 +0300

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.js
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(prices) {
+        let maxProfit = 0;
+
+        for (let i = 0; i < prices.length; i++) {
+            for (let j = i + 1; j < prices.length; j++) {
+                if (prices[i] > prices[j]) continue;
+                maxProfit = Math.max(maxProfit, prices[j] - prices[i]);
+            }
+        }
+
+        return maxProfit
+    }
+}
