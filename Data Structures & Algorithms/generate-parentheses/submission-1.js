229defd0f24cd2f713e5e688c8d36a8406c2e123
Rustam <70051506+DayTroy@users.noreply.github.com>
2026-08-24 21:01:21 +0300

    Add: generate-parentheses - submission-1

---
+++ Data Structures & Algorithms/generate-parentheses/submission-1.js
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * @param {number} n
+     * @return {string[]}
+     */
+    generateParenthesis(n) {
+        const res = [];
+        function backtrack(path, opened, closed) {
+            if (path.length === n * 2) {
+                res.push(path.join(''));
+                return;
+            }
+
+            if (opened < n) {
+                path.push('(');
+                backtrack(path, opened + 1, closed);
+                path.pop();
+            }
+
+            if (closed < opened) {
+                path.push(')');
+                backtrack(path, opened, closed + 1);
+                path.pop();
+            }
+        }
+
+        backtrack([], 0, 0);
+        return res;
+    }
+}
